6a6f643fd197abf388230863885f86c6cf69fb1a
Wunkolo <Wunkolo@gmail.com>
2017-04-13 13:47:50 -0700

    Collapse UWP header namespace

--- UWPDumper/include/UWP/UWP.hpp
+++ UWPDumper/include/UWP/UWP.hpp
@@ -4,38 +4,38 @@
 // UWP Helper class
 namespace UWP
 {
-	namespace Current
-	{
-		std::wstring GetFamilyName();
+namespace Current
+{
+std::wstring GetFamilyName();
 
-		std::wstring GetFullName();
+std::wstring GetFullName();
 
-		std::wstring GetArchitecture();
+std::wstring GetArchitecture();
 
-		std::wstring GetPublisher();
+std::wstring GetPublisher();
 
-		std::wstring GetPublisherID();
+std::wstring GetPublisherID();
 
-		std::wstring GetPackagePath();
+std::wstring GetPackagePath();
 
-		namespace Storage
-		{
-			/*
-			Apps from the same publisher may share files using the Publisher folder located at:
-			"%user_folder%\AppData\Local\Publishers\%PublisherID%\"
-			*/
-			std::wstring GetPublisherPath();
+namespace Storage
+{
+/*
+Apps from the same publisher may share files using the Publisher folder located at:
+"%user_folder%\AppData\Local\Publishers\%PublisherID%\"
+*/
+std::wstring GetPublisherPath();
 
-			// Package Storage folders
-			std::wstring GetStoragePath();
+// Package Storage folders
+std::wstring GetStoragePath();
 
-			std::wstring GetLocalCachePath();
+std::wstring GetLocalCachePath();
 
-			std::wstring GetLocalPath();
+std::wstring GetLocalPath();
 
-			std::wstring GetRoamingPath();
+std::wstring GetRoamingPath();
 
-			std::wstring GetTempStatePath();
-		}
-	}
+std::wstring GetTempStatePath();
+}
+}
 }
